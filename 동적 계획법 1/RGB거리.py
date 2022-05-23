51bf66673b7de4d006ba21313fc2b3e7273f4d29
jmkim <kimjinm0@naver.com>
2022-05-23 23:15:00 +0900

    .

---
+++ 동적 계획법 1/RGB거리.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+l = []
+
+for i in range(N):
+    l.append(list(map(int,(input().split()))))
+
+
+result = [l[0]]
+for i in range(1, N):
+    tmp = []
+    for j in range(3):
+        if j == 0:
+            cal_num = min(l[i][j] + result[i-1][j+1], l[i][j] + result[i-1][j+2])
+            tmp.append(cal_num)
+        elif j == 1:
+            cal_num = min(l[i][j] + result[i-1][j-1], l[i][j] + result[i-1][j+1])
+            tmp.append(cal_num)
+        elif j == 2:
+            cal_num = min(l[i][j] + result[i-1][j-1], l[i][j] + result[i-1][j-2])
+            tmp.append(cal_num)
+    result.append(tmp)
+
+# print(result)
+print(min(result[-1]))
