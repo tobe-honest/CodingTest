c53858396174f3fb9ad118ca0cc0ff66677570c9
jmkim <kimjinm0@naver.com>
2022-05-25 16:23:17 +0900

    first

---
+++ 동적 계획법 1/가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+l = list(map(int, input().split()))
+
+dp = [1 for _ in range(n)]
+
+for i in range(n):
+    for j in range(i):
+        if l[i] > l[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+    
+print(max(dp))
